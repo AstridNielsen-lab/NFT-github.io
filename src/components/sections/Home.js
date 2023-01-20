c1f61b0159e27219d1c30505871346fe264196fc
Astrid Nielsen Labs <32886080+AstridNielsen-lab@users.noreply.github.com>
2023-01-20 09:39:36 -0300

    vai

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import CoverVideo from '../CoverVideo'
 import TypeWriterText from '../TypeWriterText'
 import RoundTextBlack from '../../assets/Rounded-Text-Black.png'
@@ -29,18 +29,29 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 `
+const rotate = keyframes.rotate`
+100%{
+  transform: rotate(1turn);
+}
+`
 const Round = styled.div`
 position: absolute;
 bottom: 2rem;
 right: 90%;
 width: 6rem;
 height: 6rem;
+border: 1px solid ${props => props.theme.text};
+border-radius: 50%;
 
 img{
   width: 100%;
   height: auto;
+  animation: ${rotate} 6s linear infinite reverse;
 }
 `
+
+
+
 const Circle = styled.span`
 width: 3rem;
 height: 3rem;
@@ -49,9 +60,15 @@ justify-content: center;
 align-items: center;
 border-radius: 50%;
 
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+
+
 background-color: ${props => props.theme.text};
 color: ${props => props.theme.body};
-
+font-size: ${props => props.theme.fontxl};
 `
 
 
